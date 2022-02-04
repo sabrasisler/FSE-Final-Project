a21dae304107ba38ab1716a175077be456678679
Diego <artoriascreative@Diegos-MacBook-Pro.local>
2022-02-04 18:31:34 -0500

    test heroku deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/hello', (req, res) => res.send('Hello World'));
+app.get('/hello', (req, res) => res.send('Hello World 2'));
 
 const PORT = 4000;
 app.listen(PORT);
--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 const app = express();
 
-app.get('/hello', (req: Request, res: Response) => res.send('Hello World'));
+app.get('/hello', (req: Request, res: Response) => res.send('Hello World 1'));
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
