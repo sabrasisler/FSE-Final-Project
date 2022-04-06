78efa922f6b4bea49d5908eb63a505a606b7d6d4
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-04-05 20:21:55 -0400

    adding mongoose schema and model for notifications

---
+++ mongoose/notifications/NotificationSchema.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from 'mongoose';
+import MongooseException from '../../errors/MongooseException';
+import { NotificationType } from '../../models/notifications/NotificationType';
+import INotification from "../../models/notifications/INotification";
+import IUser from '../../models/users/IUser';
+import { formatJSON } from '../util/formatJSON';
+
+/**
+ * Mongoose schema for the notifications resource that takes an {@link INotifications} object. 
+ * The schema contains a user foreign key reference. All fields are required, and created/updated time stamps are added.
+ * @constructor LikeSchema
+ * @param {String} 
+ * @param {Schema.Types.ObjectId} tuit the tuit foreign key
+ * @module LikeSchema
+ *
+ */
+const NotificationSchema = new mongoose.Schema<INotification>(
+  {
+    type: { type: String, enum: NotificationType, required: true},
+    userNotified: { type: Schema.Types.ObjectId, ref: 'UserModel', required: true },
+    read: {type: Boolean}
+  },
+  {
+    timestamps: true,
+    collection: 'notifications',
+  }
+);
+
+
+formatJSON(NotificationSchema);
+export default NotificationSchema;
