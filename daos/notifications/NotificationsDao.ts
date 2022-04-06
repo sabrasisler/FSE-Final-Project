569f954e446ab2b9fbddd370a05992c0e191f134
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-04-06 12:27:23 -0400

    added DAO for notifications resource

---
+++ daos/notifications/NotificationsDao.ts
@@ -0,0 +1,33 @@
+import { Model } from 'mongoose';
+import INotification from '../../models/notifications/INotification';
+import INotificationModel from "../../mongoose/notifications/NotificationModel";
+import IDao from '../shared/IDao';
+
+/**
+ * DAO database CRUD operations for the notifications resource. Takes the injected dependencies of a {@link Model<INotification>} ORM model.
+ */
+export default class NotificationDao {
+    private static notificationDao: NotificationDao | null = null;
+    public static getInstance = (): NotificationDao => {
+        if(NotificationDao.notificationDao === null) {
+            NotificationDao.notificationDao = new NotificationDao();
+        }
+        return NotificationDao.notificationDao;
+    }
+    private constructor() {}
+    /**
+     * Finds all notifications belonging to a user id in the database. Populates the userNotified.
+     * @param {string} userId the id of the user recieving notifications.
+     * @returns an array of all notifications for the user id
+     */
+    findAllNotificationsForUser = async (userId: string): Promise<INotification[]> => 
+            INotificationModel.find({ userNotified: userId }).populate("userNotified").exec();
+
+    findAllNotifications = async (): Promise<INotification[]> => 
+        INotificationModel.find()
+        .populate("userNotified")
+        .exec();
+
+    createNotification = async (notificationType: string, uid: string): Promise<INotification> =>
+        INotificationModel.create({userNotified: uid, type: notificationType, read:false});
+}
