67550efd750a7764bddb094c2e6794b275e65cf6
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-04-11 19:46:15 -0400

    updated jsdoc for the notification schema

--- mongoose/notifications/NotificationSchema.ts
+++ mongoose/notifications/NotificationSchema.ts
@@ -2,17 +2,17 @@ import mongoose, { Schema } from 'mongoose';
 import MongooseException from '../../errors/MongooseException';
 import { NotificationType } from '../../models/notifications/NotificationType';
 import INotification from "../../models/notifications/INotification";
-import IUser from '../../models/users/IUser';
 import { formatJSON } from '../util/formatJSON';
 
 /**
  * Mongoose schema for the notifications resource that takes an {@link INotifications} object. 
  * The schema contains a user foreign key reference. All fields are required, and created/updated time stamps are added.
  * @constructor LikeSchema
- * @param {String} 
+ * @param {String} type type of notification a user is receiving
+ * @param {String} notificationString string representing the content of the notification
  * @param {Schema.Types.ObjectId} tuit the tuit foreign key
+ * @param {Boolean} read has the notification been read?
  * @module LikeSchema
- *
  */
 const NotificationSchema = new mongoose.Schema<INotification>(
   {
