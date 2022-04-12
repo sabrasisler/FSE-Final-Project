ecd88c5f4b21aa0de90fa0dd5a737d476b7062d8
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-04-11 20:05:48 -0400

    added ability to mark notification as read

--- controllers/notifications/NotificationController.ts
+++ controllers/notifications/NotificationController.ts
@@ -38,18 +38,20 @@ import NotificationDao from '../../daos/notifications/NotificationsDao';
         '/users/:userId/notifications',
         adaptRequest(this.createNotificationForUser)
       );
+      router.post('users/:userId/notifications/:nid/read')
       app.use(path, router);
       Object.freeze(this); // Make this object immutable.
     }
 
 
     createNotificationForUser = async (req: HttpRequest): Promise<HttpResponse> => {
-      const userId = req.params.userId;
+      const userNotifiedId = req.params.userId;
       const type = req.body.type;
+      const userActing = req.body.userActing;
 
       // new like
       return {
-          body: await this.notificationDao.createNotificationForUser(type, userId)
+          body: await this.notificationDao.createNotificationForUser(type, userNotifiedId, userActing)
       }
     };
 
@@ -66,99 +68,15 @@ import NotificationDao from '../../daos/notifications/NotificationsDao';
       const notifications: INotification[] = await this.notificationDao.findAllNotifications();
       return okResponse(notifications);
     };
+
+  /**
+   * Processes updating a user by calling the dao with the user id and update body from the request object. Sends the updated user object back to the client, or passes any errors to the next function middleware.
+   * @param {HttpRequest} req the request data containing client data
+   * @returns {HttpResponse} the response data to be sent to the client
+   */
+   update = async (req: HttpRequest): Promise<HttpResponse> => {
+    const nid = req.params.nid;
+    const readNotification = await this.notificationDao.updateReadNotification(nid);
+    return okResponse(readNotification);
+  };
   }
-  
-
-
-
-
-
-//  import {Express, Request, Response} from "express";
-//  import NotificationsDao from "../daos/notifications/NotificationsDao";
- 
-
-//  export default class NotificationController {
-//      private static notificationDao: NotificationsDao = NotificationsDao.getInstance();
-//      private static notificationController: NotificationController | null = null;
-
-//      public static getInstance = (app: Express): NotificationController => {
-//          if(NotificationController.notificationController === null) {
-//              NotificationController.notificationController = new NotificationController();
-//              app.get("/api/users/:uid/notifications", NotificationController.notificationController.findNotificationsForUser);
-//              app.get("/api//:tid/likes", LikeController.likeController.findAllUsersThatLikedTuit);
-//              app.put("/api/users/:uid/likes/:tid", LikeController.likeController.userTogglesTuitLikes);
-//          }
-//          return LikeController.likeController;
-//      }
- 
-//      private constructor() {}
- 
-//      /**
-//       * Retrieves all users that liked a tuit from the database
-//       * @param {Request} req Represents request from client, including the path
-//       * parameter tid representing the liked tuit
-//       * @param {Response} res Represents response to client, including the
-//       * body formatted as JSON arrays containing the user objects
-//       */
-//      findAllUsersThatLikedTuit = (req: Request, res: Response) =>
-//          LikeController.likeDao.findAllUsersThatLikedTuit(req.params.tid)
-//              .then(likes => res.json(likes));
- 
-//      /**
-//       * Retrieves all tuits liked by a user from the database
-//       * @param {Request} req Represents request from client, including the path
-//       * parameter uid representing the user liked the tuits
-//       * @param {Response} res Represents response to client, including the
-//       * body formatted as JSON arrays containing the tuit objects that were liked
-//       */
-//      findAllTuitsLikedByUser = (req: Request, res: Response) => {
-//          const uid = req.params.uid;
-//          // @ts-ignore
-//          const profile = req.session['profile'];
-//          const userId = uid === "me" && profile ?
-//              profile._id : uid;
- 
-//          LikeController.likeDao.findAllTuitsLikedByUser(userId)
-//              .then(likes => {
-//                  const likesNonNullTuits = likes.filter(like => like.tuit);
-//                  const tuitsFromLikes = likesNonNullTuits.map(like => like.tuit);
-//                  res.json(tuitsFromLikes);
-//              });
-//      }
- 
-     
-//      /**
-//       * @param {Request} req Represents request from client, including the
-//       * path parameters uid and tid representing the user that is liking the tuit
-//       * and the tuit being liked
-//       * @param {Response} res Represents response to client, including the
-//       * body formatted as JSON containing the new likes that was inserted in the
-//       * database
-//       */
-//      userTogglesTuitLikes = async (req: Request, res: Response) => {
-//          const likeDao = LikeController.likeDao;
-//          const tuitDao = LikeController.tuitDao;
-//          const uid = req.params.uid;
-//          const tid = req.params.tid;
-//          // @ts-ignore
-//          const profile = req.session['profile'];
-//          const userId = uid === "me" && profile ?
-//              profile._id : uid;
-//          try {
-//              const userAlreadyLikedTuit = await likeDao.findUserLikesTuit(userId, tid);
-//              const howManyLikedTuit = await likeDao.countHowManyLikedTuit(tid);
-//              let tuit = await tuitDao.findTuitById(tid);
-//              if (userAlreadyLikedTuit) {
-//                  await likeDao.userUnlikesTuit(userId, tid);
-//                  tuit.stats.likes = howManyLikedTuit - 1;
-//              } else {
-//                  await LikeController.likeDao.userLikesTuit(userId, tid);
-//                  tuit.stats.likes = howManyLikedTuit + 1;
-//              };
-//              await tuitDao.updateLikes(tid, tuit.stats);
-//              res.sendStatus(200);
-//          } catch (e) {
-//              res.sendStatus(404);
-//          }
-//      }
-//  };
