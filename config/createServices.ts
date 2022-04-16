1adc08a54dd6ce4566d57d9406cd9b628530145a
Diego <artoriascreative@Diegos-MacBook-Pro.local>
2022-04-16 16:40:43 -0400

    Add documentation.

--- config/createServices.ts
+++
@@ -1,6 +0,0 @@
-// import SocketService from '../services/ChatSocketService';
-// import { Server as SocketIoServer } from 'socket.io';
-// import { httpServer } from './configExpress';
-
-// const socketServer: SocketIoServer = createSocketServer(httpServer);
-// export const socketService = new SocketService(socketServer);
